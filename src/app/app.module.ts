a52b7257dee3c7f1acfd520b98db1cf868b81467
cynthia135github <c14180135@john.petra.ac.id>
2020-04-22 08:58:04 +0700

    yg Page homebarunya sdh bs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { HomeComponent } from './home/home.component';
+import { HomebaruComponent } from './homebaru/homebaru.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, HomeComponent ],
+  declarations: [ AppComponent, HelloComponent, HomebaruComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/homebaru/homebaru.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-homebaru',
+  templateUrl: './homebaru.component.html',
+  styleUrls: ['./homebaru.component.css']
+})
+export class HomebaruComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
